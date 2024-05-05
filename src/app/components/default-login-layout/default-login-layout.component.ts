ba47b503bc30cf25ed87fd8b706e21aafc1b9347
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-05-05 04:22:03 +0000

    feat: started login default layout

---
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './default-login-layout.component.html',
+  styleUrl: './default-login-layout.component.scss'
+})
+export class DefaultLoginLayoutComponent {
+  @Input() title: string = "";
+  @Input() primaryBtn: string = "";
+  @Input() secondaryBtn: string = "";
+}
