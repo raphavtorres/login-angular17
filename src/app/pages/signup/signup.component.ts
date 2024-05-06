21b947df38465024f04f4b8a059541630274f2e7
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-05-06 01:49:19 +0000

    feat: started signup page

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -6,13 +6,16 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
-interface LoginForm {
+
+interface SignupForm {
+  name: FormControl,
   email: FormControl,
-  password: FormControl
+  password: FormControl,
+  passwordConfirm: FormControl
 }
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-signup',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
@@ -22,27 +25,29 @@ interface LoginForm {
   providers: [
     LoginService
   ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
 })
-export class LoginComponent {
-  loginForm!: FormGroup<LoginForm>;
+export class SignupComponent {
+  signupForm!: FormGroup<SignupForm>;
 
   constructor(
     private router: Router,
     private loginService: LoginService,
     private toastService: ToastrService
   ) {
-    this.loginForm = new FormGroup({
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)]),
     })
   }
 
   submit() {
     this.loginService.login(
-      this.loginForm.value.email,
-      this.loginForm.value.password
+      this.signupForm.value.email,
+      this.signupForm.value.password
     ).subscribe({
       next: () => this.toastService.success("Login completed successfully!"),
       error: () => this.toastService.error("Unexpected error! Try again later...")
@@ -50,6 +55,6 @@ export class LoginComponent {
   }
 
   navigate() {
-    this.router.navigate(["signup"])
+    this.router.navigate(["login"])
   }
 }
